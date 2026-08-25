e3daa748aaf96c7e231425ce472569dba9a4d297
MrMargwa <justin_haringa@outlook.com>
2026-08-25 16:53:20 +0200

    testtt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
             if ($request->isMethod('GET')
                 && ! $request->expectsJson()
                 && ! $request->is('livewire/*')) {
-                return redirect()->to(auth()->check() ? '/admin' : '/admin/login');
+                return redirect()->to(auth()->check() ? '/dashboard' : '/dashboard/login');
             }
 
             return null;
@@ -36,6 +36,6 @@ return Application::configure(basePath: dirname(__DIR__))
                 return null;
             }
 
-            return redirect()->guest('/admin/login');
+            return redirect()->guest('/dashboard/login');
         });
     })->create();
