ea7aa1a3ac7a34c7d6dba197f322a5c30415b880
MrMargwa <justin_haringa@outlook.com>
2026-08-26 11:36:49 +0200

    test app online

--- app/Filament/Admin/Pages/Settings.php
+++ app/Filament/Admin/Pages/Settings.php
@@ -5,7 +5,7 @@ namespace App\Filament\Admin\Pages;
 use App\Models\User;
 use BackedEnum;
 use Filament\Actions\Action;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Notifications\Notification;
